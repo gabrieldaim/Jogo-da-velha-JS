e10c881c47da45e67ff74f3ee96f0db3bbba5244
Gabriel Daim <58838726+gabrieldaim@users.noreply.github.com>
2022-10-13 19:43:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const boardRegions = document.querySelectorAll('#gameBoard span')
+let vBoard = []
+let turnPlayer = ''
+
+function updateTitle() {
+    const playerInput = document.getElementById(turnPlayer)
+    document.getElementById('turnPlayer').innerText = playerInput.value
+}
+
+function initializeGame() {
+    vBoard = [['','',''],['','',''],['','','']]
+    turnPlayer = 'player1'
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+    updateTitle()
+    boardRegions.forEach(function (region) {
+        region.classList.remove('win')
+        region.innerText = ''
+        region.classList.add('cursor-pointer')
+        region.addEventListener('click', handleBoardClick)
+    })
+}
+
+function getWinRegions() {
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+      winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+      winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+      winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+      winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+      winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+      winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+      winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+      winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+  }
+    
+function disableAllRegions (){
+  boardRegions.forEach(function (region) {
+    console.log(region)
+    region.classList.remove('cursor-pointer')
+    region.removeEventListener('click', handleBoardClick)
+})
+}
+
+
+function disableRegion(region) {
+    region.classList.remove("cursor-pointer")
+    region.removeEventListener('click', handleBoardClick)
+}
+
+function handleWin(region) {
+  region.forEach(function(win) {
+    document.querySelector('[data-region="'+win+'"]').classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML = playerName + ' Venceu!'
+}
+
+function handleBoardClick(ev) {
+    const span = ev.target
+    const region = span.dataset.region
+    const rowColumnPair = region.split('.')
+    const row = rowColumnPair[0]
+    const column = rowColumnPair[1]
+    if(turnPlayer === 'player1'){
+        span.innerText = 'X'
+        vBoard[row][column] = 'X'
+    }else{
+        span.innerText = 'O'
+        vBoard[row][column] = 'O'
+    }
+    console.clear()
+    console.table(vBoard)
+
+    disableRegion(span)
+
+    let winRegions = getWinRegions()
+    if (winRegions.length   > 0) {
+        handleWin(winRegions)
+        disableAllRegions()
+    } else if(vBoard.flat().includes('')){
+        turnPlayer = turnPlayer === 'player1'? 'player2' : 'player1'
+        updateTitle()
+    }else{
+        document.querySelector('h2').innerHTML = 'EMPATE'
+    }
+}
+
+document.getElementById('start').addEventListener('click', initializeGame)
